967d69b726a13a86e62c52270c7e2ea77f0233c0
Florence Schwartz <schflo@gmail.com>
2018-04-09 15:23:25 +1000

    finished exercice

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question.include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    elsif @question == "I am going to work right now!"
+      @answer = ""
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
+
+
